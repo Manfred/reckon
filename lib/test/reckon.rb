fe0c16627cbf2328d76faed0c13996e200d17eee
manfred <manfred@35234dff-7a11-4982-9623-4bb5ff92b2a7>
2008-03-18 22:15:27 +0000

    Show the complete description path for expectations.
    git-svn-id: https://dwerg.net/svn/reckon@4 35234dff-7a11-4982-9623-4bb5ff92b2a7

--- lib/test/reckon.rb
+++ lib/test/reckon.rb
@@ -3,7 +3,7 @@ require 'test/reckon/reporter'
 
 def testing(description)
   saved = Marshal.dump instance_variables.inject({}) { |saved, name| saved[name] = instance_variable_get(name); saved }
-  @_test_description = description
+  @_test_description = @_test_description ? "#{@_test_description} #{description}" : description
   yield
   @_test_description = nil
   Marshal.load(saved).each { |name, value| instance_variable_set(name, value) }
